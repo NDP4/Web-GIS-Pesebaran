fbcfcf11bf974965dd4b3931beb5bbb6888323e9
NDP4 <nurdwipriyambodo@proton.me>
2025-07-12 00:54:53 +0700

    readme

--- config/conn.php
+++ config/conn.php
@@ -1,2 +1,6 @@
 <?php
-$conn = mysqli_connect("localhost", "yourusername", "yourpassword", "yourdatabasename");
+$conn = mysqli_connect("db", "root", "p4rv1zk44n", "uas_gis");
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
